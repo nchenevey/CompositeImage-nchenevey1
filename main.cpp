a020396ec9227ee812d89769c8f563877af4d73a
nchenevey <nchenevey1@mail.csuchico.edu>
2017-11-06 15:53:11 -0800

    Includes comments

--- main.cpp
+++ main.cpp
@@ -3,9 +3,25 @@
 #include <string>
 
 using namespace std;
+//Asks the user for file names until Done or 10 files entered
+void askUser(string);
+
+//If an image does not meet the requirements then this will print out an error
+void ccccomboBreaker(bool);
+
+//Combines the RGB values of two images
+void goPlanet(vector<vector<Pixel>>, vector<vector<Pixel>>);
+
+//Prints a progress update if the current image is successfully combined
+void ultraCombo(bool);
 
 int main()
 {
+//Ask user for file name until DONE or max files have been entered (10)(function?)
+//print an error for any file that doesn't work(function?)
+//Combine the images provided to create a composite image
+//print progress updates(function?)
+//save the composite image file
 
 
  return 0;
